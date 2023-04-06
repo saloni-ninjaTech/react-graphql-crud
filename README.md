b8a6e3957dd3cd481fde4860f53375c4acf9585f
saloni-ninjaTech <saloni@ninjatechnolabs.com>
2023-04-06 17:16:28 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# GraphQL-Social
+# GraphQL-ReactHooks
---
+++ config.js
@@ -0,0 +1,5 @@
+config = {
+  mongoUri:
+    "mongodb+srv://saloniNinja:SqYHvUUncB8tRBC2@cluster0.fkamxab.mongodb.net/?retryWrites=true&w=majority",
+};
+module.exports = config;
