0b01bc51303b30c5047dd04b9531fd6656be9ca5
saloni-ninjaTech <saloni@ninjatechnolabs.com>
2023-04-11 11:23:19 +0530

    register cache issue resolved

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ApolloClient } from "@apollo/client";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { InMemoryCache } from "@apollo/client";
 import { createHttpLink } from "apollo-link-http";
 
 const httpLink = new createHttpLink({
--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -1,98 +1,112 @@
-import React, { useState } from 'react'
-import { useMutation } from '@apollo/react-hooks'
-import { useHistory } from 'react-router-dom'
-import { REGISTER_USER } from '../queries/UserQuery'
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { useHistory } from "react-router-dom";
+import { REGISTER_USER } from "../queries/UserQuery";
 
-export const Register = () => { 
+export const Register = () => {
+  const [values, setValues] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    userName: "",
+    pass: "",
+    con_pass: "",
+  });
 
-    const [values,setValues] = useState({
-        first_name : "",
-        last_name : "",
-        email : "",
-        userName : "",
-        pass: "",
-        con_pass: ""
-    })
+  const handleChange = (prop) => (event) => {
+    setValues((prev) => ({ ...prev, [prop]: event.target.value }));
+  };
 
-    const handleChange = (prop) => (event) => {
-        setValues(prev => ({ ...prev, [prop]: event.target.value }));
-    }
+  const [error, setError] = useState(null);
+  const history = useHistory();
 
-    const [error,setError] = useState(null)
-    const history = useHistory()
+  const [register] = useMutation(REGISTER_USER, {
+    onCompleted({ registerUser }) {
+      let { err, msg } = registerUser;
+      if (err) {
+        setError(msg);
+      } else {
+        history.push("/login");
+      }
+    },
+    onError(error) {
+      console.log("An Error Occured.", error);
+    },
+  });
 
-    const [register] = useMutation(REGISTER_USER,{
-        onCompleted({registerUser}){
-            let {err, msg} = registerUser
-            if(err){
-                setError(msg)
-            }else{
-                history.push("/login")
-            }
-        },
-        onError(error){
-            console.log("An Error Occured.")
-        }
-    })
-
-    return(
-        <div className='login-flex-register'>
-            <label>First Name</label>
-            <input 
-                type='text'
-                value={values.first_name}
-                onChange={handleChange('first_name')}
-            className='input-box'></input><br/>
-            <label>Last Name</label>
-            <input 
-                type='text'
-                value={values.last_name}
-                onChange={handleChange('last_name')}
-                className='input-box'></input><br/>
-            <label>Email</label>
-            <input 
-                type='text'
-                value={values.email}
-                onChange={handleChange('email')}
-                className='input-box'></input><br/>
-            <label>UserName</label>
-            <input 
-                type='text'
-                value={values.userName}
-                onChange={handleChange('userName')}
-                className='input-box'></input><br/>
-            <label>Password</label>
-            <input 
-                type='password'
-                value={values.pass}
-                onChange={handleChange('pass')}
-                className='input-box'></input><br/>
-            <label>Confirm Password</label>
-            <input 
-                type='password'
-                value={values.con_pass}
-                onChange={handleChange('con_pass')}
-                className='input-box'></input><br/>
-            <button className='login-button2' onClick={() => {
-                if(values.pass != values.con_pass){
-                    setError("Passwords do not match.")
-                }else{
-                    register({
-                        variables :{
-                            input : {
-                                fname: values.first_name,
-                                lname: values.last_name,
-                                userName: values.userName,
-                                email: values.email,
-                                password: values.pass
-                            }
-                        }
-                    })
-                }
-            }}>
-               Register
-            </button>
-            {error? <div>{error}</div> : ""}
-        </div>
-    )
-}
+  return (
+    <div className="login-flex-register">
+      <label>First Name</label>
+      <input
+        type="text"
+        value={values.first_name}
+        onChange={handleChange("first_name")}
+        className="input-box"
+      ></input>
+      <br />
+      <label>Last Name</label>
+      <input
+        type="text"
+        value={values.last_name}
+        onChange={handleChange("last_name")}
+        className="input-box"
+      ></input>
+      <br />
+      <label>Email</label>
+      <input
+        type="text"
+        value={values.email}
+        onChange={handleChange("email")}
+        className="input-box"
+      ></input>
+      <br />
+      <label>UserName</label>
+      <input
+        type="text"
+        value={values.userName}
+        onChange={handleChange("userName")}
+        className="input-box"
+      ></input>
+      <br />
+      <label>Password</label>
+      <input
+        type="password"
+        value={values.pass}
+        onChange={handleChange("pass")}
+        className="input-box"
+      ></input>
+      <br />
+      <label>Confirm Password</label>
+      <input
+        type="password"
+        value={values.con_pass}
+        onChange={handleChange("con_pass")}
+        className="input-box"
+      ></input>
+      <br />
+      <button
+        className="login-button2"
+        onClick={() => {
+          if (values.pass != values.con_pass) {
+            setError("Passwords do not match.");
+          } else {
+            register({
+              variables: {
+                input: {
+                  fname: values.first_name,
+                  lname: values.last_name,
+                  userName: values.userName,
+                  email: values.email,
+                  password: values.pass,
+                },
+              },
+            });
+          }
+        }}
+      >
+        Register
+      </button>
+      {error ? <div>{error}</div> : ""}
+    </div>
+  );
+};
